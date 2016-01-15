4d9413767433aa7ff8dee475321e523f938a52e6
jasminejeane <jasminejeane@gmail.com>
2016-01-15 12:34:03 -0800

    first commit of Rock P S

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,13 +22,27 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
+
+if (move != undefinded){
+
+    move === move;
+}
+else {
+    move === getInput();
+}
+ // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return ;
 }
 
 function getComputerMove(move) {
+    if (move != undefinded){
+
+    move === move;
+}
+else {
+    move === getInput();
+}
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
